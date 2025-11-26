5d18fcc97d270793bf84e48b5758abcd1d19e9db
kakametom <5bvcy7rut78dfy@gmail.com>
2025-11-27 00:54:56 +0900

    コンテナにポートを開ける設定

--- develop/Kaname/vite.config.ts
+++ develop/Kaname/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // すべてのインターフェイスでリッスン
+    // 必要に応じてポート番号も設定
+    port: 5173,
+  },
 })
